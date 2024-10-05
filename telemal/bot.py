e7e01b4e90606912c54405a087e293b24aaecae0
nicolasmf <nikolamf@hotmail.com>
2024-10-06 00:17:42 +0200

    Added lots of things (Still lots to do)

---
+++ telemal/bot.py
@@ -0,0 +1,141 @@
+import sys
+import requests
+
+
+class Bot:
+    def __init__(self, token):
+        self.token = token
+        self.first_name = self.get_me(token)
+        self.json_updates = self.get_updates(token)
+        self.chat_count = len(self.get_chats())
+        self.chat_list = self.get_chats()
+
+    def get_me(self, token):
+        url = f"https://api.telegram.org/bot{token}/getMe"
+        response = requests.get(url)
+
+        if not response.json()["ok"]:
+            print("[!] Error: Couldn't get bot information. Token might be invalid.")
+            sys.exit(1)
+
+        first_name = response.json()["result"]["first_name"]
+
+        return first_name
+
+    def get_updates(self, token):
+        url = f"https://api.telegram.org/bot{token}/getUpdates"
+        response = requests.get(url)
+        return response.json()
+
+    def get_chats(self) -> str:
+        if not self.json_updates["ok"]:
+            print("[-] Error: Couldn't load chat history.")
+            return
+
+        chats = []
+
+        for chat in self.json_updates["result"]:
+            if "message" not in chat:
+                continue
+
+            chats.append(
+                f'{chat["message"]["chat"].get("title") or "Private Conversation"} > {chat["message"]["chat"]["id"]}'
+            )
+
+        chats = sorted(set(chats), key=lambda x: x.split(" > ")[0])
+
+        return chats
+
+    def parse_messages(self, chat_id):
+        if not self.json_updates["ok"]:
+            print("[-] Error: Couldn't load chat history.")
+            return
+
+        all_messages = []
+
+        for chat in self.json_updates["result"]:
+            if "message" not in chat:
+                continue
+
+            if str(chat["message"]["chat"]["id"]) != chat_id:
+                continue
+
+            username = chat["message"]["from"].get("username") or chat["message"][
+                "from"
+            ].get("first_name")
+            text = chat["message"].get("text")
+
+            if "new_chat_member" in chat["message"]:
+                all_messages.append(
+                    f'[+] {chat["message"]["new_chat_member"]["first_name"]} joined the chat.'
+                )
+
+            elif "left_chat_member" in chat["message"]:
+                left_username = chat["message"]["left_chat_member"].get(
+                    "username"
+                ) or chat["message"]["left_chat_member"].get("first_name")
+
+                if username != left_username:
+                    all_messages.append(
+                        f"[+] {username} removed {left_username} from the chat."
+                    )
+                else:
+                    all_messages.append(f"[+] {username} left the chat.")
+
+            elif "photo" in chat["message"]:
+                all_messages.append(
+                    f'{username}: [Photo] {chat["message"].get("caption") or ""}'
+                )
+
+            elif "animation" in chat["message"]:
+                all_messages.append(f"{username}: [GIF]")
+
+            elif "document" in chat["message"]:
+                all_messages.append(
+                    f'{username}: [{chat["message"]["document"]["file_name"]}] {chat["message"].get("caption") or ""}'
+                )
+
+            elif "voice" in chat["message"]:
+                all_messages.append(
+                    f"{username}: [Voice Message - {chat['message']['voice']['duration']}s]"
+                )
+
+            elif "sticker" in chat["message"]:
+                all_messages.append(f"{username}: [Sticker]")
+
+            elif "video" in chat["message"]:
+                all_messages.append(
+                    f"{username}: [Video - {chat['message']['video']['duration']}s]"
+                )
+
+            else:
+                if chat["message"]["from"]["is_bot"]:
+                    all_messages.append(f"[bot] {username}: {text}")
+                else:
+                    all_messages.append(f"{username}: {text}")
+
+        return all_messages
+
+    def get_chat_information(self, chat_id):
+        url = f"https://api.telegram.org/bot{self.token}/getChat?chat_id={chat_id}"
+        response = requests.get(url)
+
+        permissions = []
+
+        invite_link = response.json()["result"].get("invite_link") or "N/A"
+        all_permissions = response.json()["result"].get("permissions")
+
+        for permission in all_permissions:
+            if all_permissions[permission]:
+                permissions.append(permission)
+
+        url = f"https://api.telegram.org/bot{self.token}/getChatAdministrators?chat_id={chat_id}"
+        response = requests.get(url)
+
+        status = response.json()["result"][0]
+
+        for key in status:
+            if key.startswith("can_") and status[key]:
+                permissions.append(key)
+
+        return invite_link, permissions
---
+++ telemal/telemal.py
@@ -0,0 +1,169 @@
+import os
+import sys
+from bot import Bot
+
+
+def clear_screen():
+    if os.name == "nt":
+        os.system("cls")
+    else:
+        os.system("clear")
+
+
+def remove_last_lines(n=1):
+    for _ in range(n):
+        sys.stdout.write("\033[F")  # Cursor up one line
+        sys.stdout.write("\033[K")  # Clear to the end of the line
+
+
+def list_chats(bot: Bot):
+    chats = bot.chat_list
+
+    if len(chats) > 1:
+        print("[+] Multiple chats found. Please specify a chat id :")
+        for chat in chats:
+            print(f"    - {chat}")
+
+        chat_id = str(input("Chat ID > "))
+
+        while chat_id not in [chat.split(" > ")[1] for chat in chats]:
+            print("[-] Error: Invalid chat ID.")
+            chat_id = str(input("Chat ID > "))
+
+    else:
+        chat_id = chats[0]
+
+    chat_name = [
+        chat.split(" > ")[0] for chat in chats if chat.split(" > ")[1] == chat_id
+    ][0]
+
+    channel_menu(bot, chat_id, chat_name)
+
+
+def chat_history(bot: Bot, chat_id):
+    all_messages = bot.parse_messages(chat_id)
+
+    for message in all_messages:
+        print(message)
+
+
+def main_menu(token=None):
+    clear_screen()
+    logo = """
+████████╗███████╗██╗     ███████╗███╗   ███╗ █████╗ ██╗     
+╚══██╔══╝██╔════╝██║     ██╔════╝████╗ ████║██╔══██╗██║     
+   ██║   █████╗  ██║     █████╗  ██╔████╔██║███████║██║     
+   ██║   ██╔══╝  ██║     ██╔══╝  ██║╚██╔╝██║██╔══██║██║     
+   ██║   ███████╗███████╗███████╗██║ ╚═╝ ██║██║  ██║███████╗
+   ╚═╝   ╚══════╝╚══════╝╚══════╝╚═╝     ╚═╝╚═╝  ╚═╝╚══════╝
+    Telegram Bot Control Toolkit
+    
+    """
+
+    print(logo)
+
+    if not token:
+        token = input("[+] Enter bot token > ")
+        remove_last_lines()
+
+    TelegramBot = Bot(token)
+
+    print("[Main Menu]\n")
+
+    print(f"[+] Bot {TelegramBot.first_name} loaded successfully.")
+
+    if TelegramBot.chat_count > 1:
+        print(f"\033[1m[!] Bot is part of {TelegramBot.chat_count} channels\033[0m\n")
+
+        print("1. List all channels the bot is in.")
+        print("2. Enter a chat id.")
+        print("0. Exit.")
+
+        case = input("\n>>> ")
+
+        if case == "1":
+            list_chats(TelegramBot)
+        elif case == "2":
+            print("Not implemented yet.")
+        elif case == "0":
+            sys.exit(0)
+    else:
+        channel_menu(TelegramBot)
+
+    input("\n[+] Press any key to go back...")
+
+    main_menu(token)
+
+
+def channel_menu(bot: Bot, chat_id=None, chat_name=None):
+    clear_screen()
+
+    if not chat_id:
+        chat_id = bot.chat_list[0].split(" > ")[1]
+
+    if not chat_name:
+        chat_name = bot.chat_list[0].split(" > ")[0]
+
+    logo = """
+████████╗███████╗██╗     ███████╗███╗   ███╗ █████╗ ██╗     
+╚══██╔══╝██╔════╝██║     ██╔════╝████╗ ████║██╔══██╗██║     
+   ██║   █████╗  ██║     █████╗  ██╔████╔██║███████║██║     
+   ██║   ██╔══╝  ██║     ██╔══╝  ██║╚██╔╝██║██╔══██║██║     
+   ██║   ███████╗███████╗███████╗██║ ╚═╝ ██║██║  ██║███████╗
+   ╚═╝   ╚══════╝╚══════╝╚══════╝╚═╝     ╚═╝╚═╝  ╚═╝╚══════╝
+    Telegram Bot Control Toolkit
+    
+    """
+
+    print(logo)
+
+    print(f"[Channel: {chat_name}]\n")
+
+    invite_link, permissions = bot.get_chat_information(chat_id)
+
+    for index, permission in enumerate(permissions):
+        if permission in ["can_send_messages", "can_delete_messages"]:
+            permissions[index] = f"\033[1m{permission}\033[0m"
+
+    print(f"[+] Chat ID: {chat_id}")
+    print(f"[+] Invite Link: {invite_link}")
+    print(f"[+] Permissions:", end=" ")
+
+    print(*sorted(permissions), sep=", ")
+
+    print("")
+
+    print("1. List all messages.")
+    print("2. Send a message.")
+    print("3. Send a file.")
+    print("3. Download files.")
+    print("4. Delete all messages.")
+    print("5. Export all text messages.")
+    print("6. Leave channel.")
+    print("0. Go back.")
+
+    case = input("\n>>> ")
+    print()
+
+    if case == "1":
+        chat_history(bot, chat_id)
+
+        input("\n[+] Press any key to go back...")
+    elif case == "2":
+        print("Not implemented yet.")
+    elif case == "3":
+        print("Not implemented yet.")
+    elif case == "4":
+        print("Not implemented yet.")
+    elif case == "5":
+        print("Not implemented yet.")
+    elif case == "0":
+        main_menu(bot.token)
+    else:
+        print("[!] Invalid option.")
+        input("[+] Press any key to go back...")
+
+    channel_menu(bot, chat_id, chat_name)
+
+
+main_menu()
