6ad214744c1a47753138f95eebf8925dd5720556
nicolasmf <nikolamf@hotmail.com>
2024-10-14 00:51:02 +0200

    Updated setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,18 @@
-from setuptools import setup
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
 
 setup(
     name="telemal",
-    version="1.2",
+    version="1.2.1",
     description="Get intelligence on malicious Telegram channels using bot Token",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     url="https://github.com/nicolasmf/Telemal",
     author="TA1GA",
     license="MIT",
-    packages=["telemal"],
+    packages=find_packages(),
     entry_points={
         "console_scripts": ["telemal=telemal.telemal:main"],
     },
